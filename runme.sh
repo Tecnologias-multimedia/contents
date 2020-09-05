6bb2814637975a4e32beb966f57306fadd080d50
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-09-05 22:38:56 +0200

    .

--- runme.sh
+++ runme.sh
@@ -6,6 +6,8 @@ grep citation ~/the_sound/index.aux >> citations
 grep citation ~/human_auditory_system/index.aux >> citations
 grep citation ~/human_sound_perception/index.aux >> citations
 grep citation ~/the_light/index.aux >> citations
+grep citation ~/human_visual_system/index.aux >> citations
+grep citation ~/human_light_perception/index.aux >> citations
 cat citations >> index.aux
 bibtex index
 make index.html
